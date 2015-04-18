56793740f18050bfadeace6a2ccb2e70e322f264
istinspring <istinspring@gmail.com>
2015-04-18 18:37:22 +0700

    add divshot and deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
 var webserver = require('gulp-webserver');
+
+
+gulp.task('deploy', function() {
+  return gulp.src(['./src/**/*', './src/*'])
+             .pipe(ghPages());
+});
+
 
 gulp.task('webserver', function() {
   gulp.src('./src')
